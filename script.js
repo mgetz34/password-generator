5c28daeaad1fffe3d8c61fc4d43fa65fcb9e873e
mgetz34 <mgetz340@gmail.com>
2022-10-27 16:39:17 -0600

    edited js variables and alert messages

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+//generated alert messages 
+
+alert("New Password Length? Must be between 8-128 characters")
+
+alert("Include lowercase characters?")
+
+alert("Include uppercase characters?")
+
+alert("Include numbers?")
+
+alert("Include special characters?")
+
+//generated password variables 
+
+var passwordLengthMin = (8);
+
+var passwordLengthMax = (128);
+
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=",
+"+", "[", "]", "{", "}", ";", ":", "<", ">", "?", "/", "?", "|", "/", ".", ","];
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +42,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
